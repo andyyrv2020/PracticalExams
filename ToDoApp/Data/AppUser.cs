1b8ff1b43865bc4fed1603c09e6b88059f3d70a1
Андрей Рашев <ar42698937@edu.mon.bg>
2024-01-27 11:43:32 +0200

    Added the first 4 properties for the app

--- ToDoApp/Data/AppUser.cs
+++ ToDoApp/Data/AppUser.cs
@@ -1,8 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
 
 namespace ToDoApp.Data
 {
     public class AppUser : IdentityUser
     {
+        public AppUser()
+        {
+            Lists = new List<ToDoList>();
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public virtual List<ToDoList> Lists { get; set; }
+
     }
 }
